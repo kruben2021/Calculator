afce229278bc431712c5b3152fc3f0648e0072b1
NAIDOOK <NAIDOOK@CT-M-FID-IT-KN.ADS.CPUT.AC.ZA>
2021-04-27 21:01:13 +0200

    modified integerSum with comments add doubleSum

--- src/main/java/za/ac/cput/calculator/Addition.java
+++ src/main/java/za/ac/cput/calculator/Addition.java
@@ -7,7 +7,14 @@ package za.ac.cput.calculator;
 
 
 public class Addition {
+    
+    // This adds two integers
     public static int integerSum(int a, int b) {
+        return (a + b);
+    }
+    
+    // This adds two real numbers
+    public static double doubleSum(double a, double b) {
         return (a + b);
     }
 }
